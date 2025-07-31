ebe61a2e0556bd3a4b5d52ce10e0c93594606c0b
honga789 <honganhnguyenpro655@gmail.com>
2025-08-01 00:56:36 +0700

    update for A5000x2

--- cifar10-test-sym40.py
+++ cifar10-test-sym40.py
@@ -648,6 +648,7 @@ if __name__ == '__main__':
         "image_dir": "./Data/Cifar10-test/images",
         "data_type": "image",      # 'image' hoặc 'text'
         "batch_size": 128,         # Mặc định từ src gốc
+        "num_workers": 8,          # Tham số num_workers
         "num_epochs": 200,         # Mặc định từ src gốc
         "es": None,                  # Đặt None để tự động ước tính, hoặc điền số nguyên (vd: 40)
         "alpha": 0.9,              # Mặc định từ src gốc
@@ -682,7 +683,8 @@ if __name__ == '__main__':
         csv_path=config["csv_path"], feather_path=config["feather_path"],
         data_column=config["data_column"], label_column=config["label_column"],
         image_dir=config["image_dir"], data_type=config["data_type"],
-        batch_size=config["batch_size"]
+        batch_size=config["batch_size"],
+        num_workers=config["num_workers"]
     )
     trainloader, noisy_labels, clean_labels = loader.run()
     num_classes = int(np.max(clean_labels)) + 1
